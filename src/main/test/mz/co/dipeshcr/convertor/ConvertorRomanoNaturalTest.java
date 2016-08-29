30931ad553dbefd2bec0a00b505b0d95053d8d17
DiCR <dicr@gmail.com>
2016-08-29 22:40:12 +0200

    Teste para o cenario: Converter numero romano II para 2

--- src/main/test/mz/co/dipeshcr/convertor/ConvertorRomanoNaturalTest.java
+++ src/main/test/mz/co/dipeshcr/convertor/ConvertorRomanoNaturalTest.java
@@ -4,11 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class ConvertorRomanoNaturalTest {
-	
+
 	@Test
 	public void deveConverterNumeroRomano_I_paraNumeRoNatural_1() {
 		ConvertorRomanoNatural convertor = new ConvertorRomanoNatural();
 		Assert.assertEquals(1, convertor.numeroRomanoParanumeroNatural("I"));
 
 	}
+	
+	@Test
+	public void deveConverterNumeroRomano_II_paraNumeRoNatural_2() {
+		ConvertorRomanoNatural convertor = new ConvertorRomanoNatural();
+		Assert.assertEquals(2, convertor.numeroRomanoParanumeroNatural("II"));
+
+	}
+
+
 }
