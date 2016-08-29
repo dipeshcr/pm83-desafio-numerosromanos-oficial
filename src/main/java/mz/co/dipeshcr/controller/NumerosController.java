580e79f47d5d8016f5978835cc118962d5416c8f
DiCR <dicr@gmail.com>
2016-08-29 23:21:11 +0200

    Definindo o RequestMapping por forma a mostrar a pagina que exibe o resultado final da conversao de um numero romano para um numero natural

--- src/main/java/mz/co/dipeshcr/controller/NumerosController.java
+++ src/main/java/mz/co/dipeshcr/controller/NumerosController.java
@@ -1,7 +1,11 @@
 package mz.co.dipeshcr.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import mz.co.dipeshcr.modelo.NumeroRomano;
 
 @Controller
 public class NumerosController {
@@ -10,6 +14,15 @@ public class NumerosController {
 	public String mostraPaginaInicial() {
 		System.out.println("Executando a logica com o Spring MVC");
 		return "index";
+	}
+
+	@RequestMapping("/converterNumerosRomanos")
+	public String converterNumeroRomano(NumeroRomano numeroRomano, HttpSession session) {
+		System.out.println("Executando a logica com o Spring MVC");
+		int numero = numeroRomano.getNumeroNatural(numeroRomano.getNumeroRomano());
+		session.setAttribute("numeroRomano", numeroRomano);
+		session.setAttribute("numero", numero);
+		return "resultadofinal";
 	}
 
 }
