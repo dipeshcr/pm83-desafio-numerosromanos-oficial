9dadbeaa3ac8f188221602ff0f547e6c6e619c2e
DiCR <dicr@gmail.com>
2016-08-29 22:32:33 +0200

    Implementando a logica de conversao de numero romano para natural, por forma a passar no cenario de Teste: Converter numero romano I para 1

--- src/main/java/mz/co/dipeshcr/convertor/ConvertorRomanoNatural.java
+++ src/main/java/mz/co/dipeshcr/convertor/ConvertorRomanoNatural.java
@@ -2,8 +2,64 @@ package mz.co.dipeshcr.convertor;
 
 public class ConvertorRomanoNatural {
 
-	public Object numeroRomanoParanumeroNatural(String string) {
-		return null;
+	public int numeroRomanoParanumeroNatural(String numero) {
+		int numeroAtual = 0;
+		int ultimoNumero = 0;
+
+		String numeroRomano = numero.toUpperCase();
+
+		for (int i = numeroRomano.length() - 1; i >= 0; i--) {
+
+			char numeroRomanoAtual = numeroRomano.charAt(i);
+
+			switch (numeroRomanoAtual) {
+			case 'I':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(1, ultimoNumero, numeroAtual);
+				ultimoNumero = 1;
+				break;
+
+			case 'V':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(5, ultimoNumero, numeroAtual);
+				ultimoNumero = 5;
+				break;
+
+			case 'X':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(10, ultimoNumero, numeroAtual);
+				ultimoNumero = 10;
+				break;
+
+			case 'L':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(50, ultimoNumero, numeroAtual);
+				ultimoNumero = 50;
+				break;
+
+			case 'C':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(100, ultimoNumero, numeroAtual);
+				ultimoNumero = 100;
+				break;
+
+			case 'D':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(500, ultimoNumero, numeroAtual);
+				ultimoNumero = 500;
+				break;
+
+			case 'M':
+				numeroAtual = converterNumeroRomanoAtualParaNumeroNatural(1000, ultimoNumero, numeroAtual);
+				ultimoNumero = 1000;
+				break;
+
+			}
+		}
+
+		return numeroAtual;
+	}
+
+	private int converterNumeroRomanoAtualParaNumeroNatural(int numeroAtual, int ultimoNumero, int ultimoNumeroAtual) {
+		if (ultimoNumero > numeroAtual) {
+			return ultimoNumeroAtual - numeroAtual;
+		} else {
+			return ultimoNumeroAtual + numeroAtual;
+		}
 	}
 
 }
